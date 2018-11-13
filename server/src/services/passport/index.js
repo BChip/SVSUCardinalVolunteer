7ea4014ffa8a568d2e7e01a785b96a7c34894e92
Brad <bachippi@svsu.edu>
2018-11-12 19:19:28 -0500

    Get rid of master key middleware

--- server/src/services/passport/index.js
+++ server/src/services/passport/index.js
@@ -91,14 +91,6 @@ passport.use('google', new BearerStrategy((token, done) => {
   }).catch(done)
 }))
 
-passport.use('master', new BearerStrategy((token, done) => {
-  if (token === masterKey) {
-    done(null, {})
-  } else {
-    done(null, false)
-  }
-}))
-
 passport.use('token', new JwtStrategy({
   secretOrKey: jwtSecret,
   jwtFromRequest: ExtractJwt.fromExtractors([
