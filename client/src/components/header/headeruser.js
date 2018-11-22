3eafddda0908645dfce72ef2852a608ac783f16a
sailendra karki <sailendra.karki@gmail.com>
2018-11-22 00:22:20 -0500

    fixes minor issues

--- client/src/components/header/headeruser.js
+++ client/src/components/header/headeruser.js
@@ -8,7 +8,7 @@ const Userheader = () => (
     { /* PROFILE DROPDOWN - scrolling off the page to the right */ }
     <li className="nav-item dropdown">
       <Link to="/Postings" className="nav-link dropdown-toggle" id="navDropDownLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <img src={localStorage.getItem('picture')} width="60" height="40" alt={localStorage.getItem('name')} />
+        <img src={localStorage.getItem('picture')} width="60" height="40" alt={localStorage.getItem('name')} className="imgresponsive" />
       </Link>
       <div className="dropdown-menu" aria-labelledby="navDropDownLink">
         <Link className="dropdown-item" to={`/userview/${localStorage.getItem('id')}`}>{localStorage.getItem('name')}</Link>
